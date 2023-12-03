77e719ffcf2f75c256bfc113778d5ee9dd2e0076
Michael Toner <22949655+mtoner23@users.noreply.github.com>
2023-12-03 01:59:39 -0600

    still not correct, debug

--- aoc3/src/main.rs
+++ aoc3/src/main.rs
@@ -1,6 +1,7 @@
+// use regex::Regex;
 use std::fs;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 struct Location {
     x: usize,
     y: usize,
@@ -9,72 +10,105 @@ struct Location {
 fn main() {
     // println!("Hello, world!");
 
-    let filepath = "src/test.txt";
+    let filepath = "src/input2.txt";
     println!("in file {} ", filepath);
 
     let file = fs::read_to_string(filepath).unwrap();
 
+    // let num_regex = Regex::new(r"\d+").unwrap();
+
     // let height = file.lines().count();
     // let width = file.lines().nth(0).unwrap().len();
 
-    let mut symbols: Vec<Location> = vec![];
-    let mut numbers: Vec<(Location, Location)> = vec![];
+    let mut symbols: Vec<(char, Location)> = vec![];
+    let mut numbers: Vec<(usize, Location, Location)> = vec![];
 
     let mut num: String = "".to_owned();
+
+    // let mut total_nums: Vec<usize> = vec![];
+
+    // for l in file.lines() {
+    //     let line_nums = &num_regex.captures(l).unwrap();
+    //     // let nums = line_nums["num"];
+    //     for n in line_nums.iter() {
+    //         println!("{}", n.unwrap().as_str());
+    //     }
+    // }
 
     for (l, line) in file.lines().enumerate() {
         for (c, character) in line.chars().enumerate() {
             if character.is_numeric() {
                 if num.len() == 0 {
-                    numbers.push((Location { x: c, y: l }, Location { x: 0, y: 0 }));
+                    numbers.push((0, Location { x: c, y: l }, Location { x: 0, y: 0 }));
                 }
                 num.push(character);
             } else {
                 if num.len() != 0 {
-                    numbers.last_mut().unwrap().1 = Location { x: c, y: l };
+                    numbers.last_mut().unwrap().0 = num.parse().unwrap();
+                    numbers.last_mut().unwrap().2 = Location { x: c - 1, y: l };
                 }
                 num = "".to_owned();
                 if character != '.' {
-                    symbols.push(Location { x: c, y: l });
+                    symbols.push((character, Location { x: c, y: l }));
+                }
+            }
+        }
+        num = "".to_owned();
+    }
+
+    let mut part_nums: Vec<(usize, Location)> = vec![];
+
+    for s in &symbols {
+        for n in &numbers {
+            let x0_diff = if s.1.x > n.1.x {
+                s.1.x - n.1.x
+            } else {
+                n.1.x - s.1.x
+            };
+            let x1_diff = if s.1.x > n.2.x {
+                s.1.x - n.2.x
+            } else {
+                n.2.x - s.1.x
+            };
+            let y0_diff = if s.1.y > n.1.y {
+                s.1.y - n.1.y
+            } else {
+                n.1.y - s.1.y
+            };
+            let y1_diff = if s.1.y > n.2.y {
+                s.1.y - n.2.y
+            } else {
+                n.2.y - s.1.y
+            };
+
+            if x0_diff <= 1 && y0_diff <= 1 {
+                if !part_nums.contains(&(n.0, n.1.clone())) {
+                    // println!("Found Begin: s {:?}, n {:?}", s, n);
+                    part_nums.push((n.0, n.1.clone()));
+                } else {
+                    println!("Duplicate: {:?}", n);
+                }
+            } else if x1_diff <= 1 && y1_diff <= 1 {
+                if !part_nums.contains(&(n.0, n.1.clone())) {
+                    // println!("Found End  : s {:?}, n {:?}", s, n);
+                    part_nums.push((n.0, n.1.clone()));
+                } else {
+                    println!("Duplicate: {:?}", n);
                 }
             }
         }
     }
+    for n in &numbers {
+        // println!("{:?}", n);
+    }
+    // for s in symbols {
+    //     println!("{:?}", s);
+    // }
+    let mut sum = 0;
+    for p in &part_nums {
+        sum += p.0;
+        // println!("parts {:?}, sum {sum}", p)
+    }
 
-    for s in &symbols {
-        for n in &numbers {
-            let x0_diff = if s.x > n.0.x {
-                s.x - n.0.x
-            } else {
-                n.0.x - s.x
-            };
-            let x1_diff = if s.x > n.1.x {
-                s.x - n.1.x
-            } else {
-                n.1.x - s.x
-            };
-            let y0_diff = if s.x > n.0.y {
-                s.x - n.0.y
-            } else {
-                n.0.y - s.x
-            };
-            let y1_diff = if s.x > n.1.y {
-                s.x - n.1.y
-            } else {
-                n.1.y - s.x
-            };
-
-            if x0_diff <= 1 && y0_diff <= 1 {
-                println!("Found Begin: s {:?}, n {:?}", s, n.0);
-            } else if x1_diff <= 1 && y1_diff <= 1 {
-                println!("Found End  : s {:?}, n {:?}", s, n.1);
-            }
-        }
-    }
-    for n in numbers {
-        println!("{:?}", n);
-    }
-    for s in symbols {
-        println!("{:?}", s);
-    }
+    print!("{sum}");
 }
