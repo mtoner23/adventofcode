dd12358d635dae082c217e1177f14eabd06624cd
Michael Toner <22949655+mtoner23@users.noreply.github.com>
2025-12-08 13:49:51 -0600

    part5 done!

--- 2025/aoc5/src/main.rs
+++ 2025/aoc5/src/main.rs
@@ -2,17 +2,37 @@ use regex::Regex;
 use std::error::Error;
 use std::fs;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Range {
     start: u64,
     end: u64,
+}
+
+fn get_range_size(r: &Range) -> u64 {
+    return r.end - r.start + 1;
 }
 
 fn in_range(r: &Range, v: u64) -> bool {
     return v >= r.start && v <= r.end;
 }
 
+fn merge_ranges(r1: &Range, r2: &Range) -> Option<Range> {
+    if r1.end + 1 < r2.start || r2.end + 1 < r1.start {
+        return None;
+    }
+
+    let start = std::cmp::min(r1.start, r2.start);
+    let end = std::cmp::max(r1.end, r2.end);
+
+    return Some(Range { start, end });
+}
+
+fn validate_range(r: &Range) -> bool {
+    return r.start <= r.end;
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let filepath = "src/test.txt";
+    let filepath = "src/input.txt";
     let file = fs::read_to_string(filepath)?;
 
     let mut lines = file.lines();
@@ -32,34 +52,73 @@ fn main() -> Result<(), Box<dyn Error>> {
             let end: u64 = caps.get(2).unwrap().as_str().parse().unwrap();
             ranges.push(Range { start, end });
 
-            // println!("Range: {}-{}", range.start, range.end);
+            if !validate_range(ranges.last().unwrap()) {
+                panic!("Invalid range: {}-{}", start, end);
+            }
         } else {
             break;
         }
     }
 
-    // lines.next();
+    let mut new_ranges: Vec<Range> = ranges.clone();
 
-    let mut count = 0;
-
-    for l in lines {
-        let v: u64 = l.parse().unwrap();
-        let mut found = false;
-        for r in &ranges {
-            if in_range(r, v) {
-                found = true;
+    loop {
+        let mut changes = false;
+        for r1 in 0..ranges.len() {
+            for r2 in r1 + 1..ranges.len() {
+                let range1 = &ranges[r1];
+                let range2 = &ranges[r2];
+                if let Some(merged) = merge_ranges(range1, range2) {
+                    println!(
+                        "Merging {}-{} and {}-{} into {}-{}",
+                        range1.start,
+                        range1.end,
+                        range2.start,
+                        range2.end,
+                        merged.start,
+                        merged.end
+                    );
+                    new_ranges[r1] = merged;
+                    new_ranges.remove(r2);
+                    changes = true;
+                    break;
+                }
+            }
+            if changes {
                 break;
             }
         }
-        if found {
-            count += 1;
-            // println!("{}: YES", v);
+        // println!("New ranges: {:?}", new_ranges);
+        if !changes {
+            break;
         } else {
-            // println!("{}: NO", v);
+            ranges = new_ranges.clone();
         }
     }
 
+    let count = ranges.iter().map(|r| get_range_size(r)).sum::<u64>();
+
     println!("Count: {}", count);
+
+    //Part 1
+    // let mut count = 0;
+
+    // for l in lines {
+    //     let v: u64 = l.parse().unwrap();
+    //     let mut found = false;
+    //     for r in &ranges {
+    //         if in_range(r, v) {
+    //             found = true;
+    //             break;
+    //         }
+    //     }
+    //     if found {
+    //         count += 1;
+    //         // println!("{}: YES", v);
+    //     } else {
+    //         // println!("{}: NO", v);
+    //     }
+    // }
 
     return Ok(());
 }
