20960023107c0bbfe24448aa0111934c06369f02
Michael Toner <22949655+mtoner23@users.noreply.github.com>
2025-12-12 07:25:03 -0600

    aoc11 donega 1!

--- 2025/aoc11/src/main.rs
+++ 2025/aoc11/src/main.rs
@@ -2,12 +2,15 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::{fs, vec};
 
-const START: &str = "svr";
-const END: &str = "out";
+const START: &str = "fft";
+const END: &str = "dac";
 
 // should find fft around ~800,000
+// total 152973158478561669
 // svr fft dac out
-//
+// svr -> fft = 2733
+// fft -> dac = 4199340
+// dac -> out = 25644
 
 #[derive(Clone, Debug, PartialEq)]
 struct Label {
@@ -24,8 +27,39 @@ fn print_queue(queue: &Vec<(Label, Vec<String>)>) {
     }
 }
 
+fn visit_label(idx: usize, labels: &Vec<Label>, map: &mut HashMap<String, Option<i64>>) -> i64 {
+    let l = labels[idx].clone();
+    // let map_lookup = map.get(&l.name).unwrap();
+    let mut count = 0;
+    for i in 0..l.outs.len() {
+        let n_idx = l.outs[i];
+        let name = l.out_names[i].clone();
+        match n_idx {
+            Some(n) => {
+                let dest = labels[n].name.clone();
+                let dest_lookup = map.get(&dest).unwrap();
+                count += match dest_lookup {
+                    Some(i) => *i,
+                    None => {
+                        println!("loop {}", n);
+                        visit_label(n, labels, map)
+                    }
+                }
+            }
+            // None => count += 1,
+            None => {
+                if name == END {
+                    count += 1
+                }
+            }
+        }
+    }
+    map.insert(l.name, Some(count));
+    return count;
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let filepath = "src/test2.txt";
+    let filepath = "src/input.txt";
     let file = fs::read_to_string(filepath)?;
 
     let lines = file.lines();
@@ -63,7 +97,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     for i in 0..labels.len() {
         // let l = &labels[i];
         for o in labels[i].out_names.clone() {
-            if o == "out" {
+            if o == "out" || o == END {
                 labels[i].outs.push(None);
             } else {
                 // println!("Searching for {}", o);
@@ -83,67 +117,74 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut queue: Vec<(Label, Vec<String>)> = vec![];
 
     let mut next: (Label, Vec<String>) = (start.clone(), vec![]);
-    // let mut count: i64 = 0;
 
     // let mut this_path: Vec<String> = vec![];
-    let mut i = 0;
-    let mut start_time = std::time::Instant::now();
+    // let mut i = 0;
+    // let mut start_time = std::time::Instant::now();
 
-    loop {
-        let next_idxs = next.0.outs.clone();
-        let mut path = next.1.clone();
+    let count = visit_label(start.idx, &labels, &mut map);
+    println!("count {}", count);
 
-        let mut discovered = true;
-        let mut count = 0;
-        for n in next_idxs {
-            match n {
-                Some(i) => {
-                    let name = labels[i].name.clone();
-                    let get = map.get(&name).unwrap();
+    // loop {
+    //     let next_idxs = next.0.outs.clone();
+    //     let mut path = next.1.clone();
 
-                    print!("Label {}", name);
+    //     let mut discovered = true;
+    //     let mut count = 0;
+    //     print!("Start {}, ", next.0.name);
+    //     for n in next_idxs {
+    //         match n {
+    //             Some(i) => {
+    //                 let name = labels[i].name.clone();
+    //                 let get = map.get(&name).unwrap();
 
-                    if get.is_none() {
-                        println!(" Unmapped");
-                        discovered = false;
-                        path.push(name);
-                        queue.push((labels[i].clone(), path.clone()));
-                    } else {
-                        println!(" Unmapped");
-                        count += get.unwrap();
-                    }
-                }
-                None => {
-                    count += 1;
-                    // if next.0.name == END {
-                    // }
-                }
-            }
-        }
-        if discovered {
-            map.insert(next.0.name, Some(count));
-        }
+    //                 print!("Label {}, ", name);
 
-        // print_queue(&queue);
-        println!("Map: {:?}", map);
-        match queue.pop() {
-            Some(n) => next = n,
-            None => break,
-        };
-        i += 1;
-        if i % 1000 == 0 {
-            let elapsed = start_time.elapsed();
-            println!(
-                "iter {}, path size {}: - elapsed: {:?}",
-                i,
-                path.len(),
-                elapsed
-            );
-            start_time = std::time::Instant::now();
-            // break;
-        }
-    }
-    println!("Count: {:?}", map.get(START).unwrap());
+    //                 if get.is_none() {
+    //                     print!("Unmapped , ");
+    //                     discovered = false;
+    //                     path.push(name);
+    //                     queue.push((labels[i].clone(), path.clone()));
+    //                 } else {
+    //                     print!("Allmapped, ");
+    //                     // count += get.unwrap();
+    //                     map.insert(next.0.name.clone(), get.unwrap());
+    //                 }
+    //             }
+    //             None => {
+    //                 count += 1;
+    //             }
+    //         }
+    //     }
+    //     if discovered {
+    //         println!("discov true");
+    //         // map.insert(next.0.name, Some(count));
+    //     } else {
+    //         println!("discov false");
+    //         // map.insert(next.0.name, Some(count));
+    //     }
+
+    //     // print_queue(&queue);
+    //     // println!("Map: {:?}", map);
+    //     match queue.pop() {
+    //         Some(n) => next = n,
+    //         None => break,
+    //     };
+    //     i += 1;
+    //     if i % 1000 == 0 {
+    //         let elapsed = start_time.elapsed();
+    //         println!(
+    //             "iter {}, path size {}: - elapsed: {:?}",
+    //             i,
+    //             path.len(),
+    //             elapsed
+    //         );
+    //         start_time = std::time::Instant::now();
+    //         // break;
+    //     }
+    // }
+    // println!("Map: {:?}", map);
+    // println!("Count: {:?}", map.get(START).unwrap());
 
     return Ok(());
 }
